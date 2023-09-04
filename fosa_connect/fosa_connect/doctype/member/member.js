f1e549888fc2e20a2f717a878f1f63254b0f2239
nidh-eesh <nidhinmanu07@gmail.com>
2023-09-04 17:14:06 +0530

    feat:Added fields under Profile tab

--- fosa_connect/fosa_connect/doctype/member/member.js
+++ fosa_connect/fosa_connect/doctype/member/member.js
@@ -2,10 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Member', {
-	// refresh: function(frm) {
+    setup: function(frm) {
+        frm.set_query('primary_address', function(doc) {
+			return {
+				filters: {
+					'link_doctype': 'Member',
+					'link_name': doc.name
+				}
+			}
+		})
+    },
+	 refresh: function(frm) {
+        frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Member'}
 
-	// }
+        if(!frm.doc.__islocal) {
+			frappe.contacts.render_address_and_contact(frm);
+        } else {
+			frappe.contacts.clear_address_and_contact(frm);
+		}
+        //frm.toggle_display(['address_html'], !frm.doc.__islocal);
+        frm.set_df_property('primary_address', 'reqd', 1)
+	 },
+    
 	validate: function (frm) {
+        if(frm.doc.status == 'Student'){
         var year = frm.doc.year_of_admission;
 
         // Check if the year is a valid four-digit number
@@ -24,4 +44,5 @@ frappe.ui.form.on('Member', {
             }
         }
     }
+}
 });
