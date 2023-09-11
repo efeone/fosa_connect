73f65568081a56341e16d63f84ccbb7d184f95d5
aswanikrishnap <applermon207@gmail.com>
2023-09-11 10:35:57 +0530

    feat: create Web view of job doctype

--- fosa_connect/fosa_connect/doctype/job/job.js
+++ fosa_connect/fosa_connect/doctype/job/job.js
@@ -1,33 +1,45 @@
 frappe.ui.form.on('Job', {
   refresh: function (frm) {
-    if (frappe.user_roles.includes('Student')){
+    //Only Admin can view the field
+      let roles = frappe.user_roles;
+      if (roles.includes('Student')) {
+            // Check if the job is disabled
+            if (frm.doc.status === 'Disabled') {
+                // Hide the "Status" field
+                frm.set_df_property('status', 'hidden', 1);
+            }
+        }
+    if(roles.includes("Administrator")||roles.includes("Alumni")){
+      frm.set_df_property('disabled','hidden',0);
+      frm.set_df_property('published','hidden',0);
+    }
+    else{
+      frm.set_df_property('disabled','hidden',1);
+      frm.set_df_property('published','hidden',1);
+    }
+    if (roles.includes('Student')){
     if (!frm.is_new()){
 
       var liked_user_list = []
-      
+
       if (frm.doc._liked_by) {
-        liked_user_list = JSON.parse(frm.doc._liked_by)}
 
-      // Checking if the logged in user has liked the job
-      if (!liked_user_list.includes(frappe.session.user)){
+      var liked_user_list = JSON.parse(frm.doc._liked_by)
 
-        frm.add_custom_button(('Interested'), function () {
-          create_job_interest(frm)
-          interest_button_fn(frm, `Yes`)
-        });
-      }
-      else {
-        frm.add_custom_button('Not Interested', function () {
-          interest_button_fn(frm, `No`)
-        });
-      }
+    // Checking if the logged in user has liked the job
+    if (!liked_user_list.includes(frappe.session.user)){
+
+      frm.add_custom_button(('Interested'), function () {
+        interest_button_fn(frm, `Yes`)
+      });
     }
+    else {
+      frm.add_custom_button('Not Interested', function () {
+        interest_button_fn(frm, `No`)
+      });
     }
-  }
-  
-  
-});
-
+  }}}
+}});
 function interest_button_fn(frm, add) {
   // Calls a server side method to add/remove the logged in user in the liked list of the document
   frappe.call({
@@ -42,25 +54,5 @@ function interest_button_fn(frm, add) {
         frm.reload_doc();
       }
     },
-  });
-}
-
-function create_job_interest(frm) {
-// Create a new entry in the "Job Interest" DocType
-  frappe.call({
-    method: 'fosa_connect.fosa_connect.utils.create_job_interest_entry',
-    args: {
-        job_id: frm.doc.name,
-        student_id: frappe.session.user
-    },
-    callback: function(response) {
-        if (response.message === 'success') {
-            // Successfully created the "Job Interest" entry
-            frappe.msgprint('You have expressed interest in this job.');
-        } else {
-            // Handle any errors that occurred during the creation of the entry
-            frappe.msgprint('An error occurred while expressing interest in this job.');
-        }
-    }
   });
 }
