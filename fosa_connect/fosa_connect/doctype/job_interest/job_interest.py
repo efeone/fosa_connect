26e55e9b70b05cdc1257975a3b88bf6a7692c426
Jishna-tp <jishnatp7@gmail.com>
2023-09-11 10:25:48 +0530

    feat: Created web view for job interest doctype

--- fosa_connect/fosa_connect/doctype/job_interest/job_interest.py
+++ fosa_connect/fosa_connect/doctype/job_interest/job_interest.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class JobInterest(Document):
+class JobInterest(WebsiteGenerator):
 	pass
