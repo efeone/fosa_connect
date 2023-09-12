a626faa160ce5f190dcaa1adf08ad06f91081666
nidh-eesh <nidhinmanu07@gmail.com>
2023-09-12 20:20:44 +0530

    feat: Added feature to send notification to Alumni

--- fosa_connect/fosa_connect/doctype/job_interest/job_interest.py
+++ fosa_connect/fosa_connect/doctype/job_interest/job_interest.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2023, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
+from frappe.model.document import Document
+from fosa_connect.fosa_connect.utils import create_notification_log
 
 class JobInterest(WebsiteGenerator):
-	pass
+    pass
+
+
+class JobInterest(Document):
+    def after_insert(self):
+       if self.job:
+           recipient = frappe.db.get_value('Job', self.job, 'owner')
+           job_title = frappe.db.get_value('Job', self.job, 'job_title')
+           job_create_notification_log(self, recipient, job_title) 
+
+# Job Update for alumni
+@frappe.whitelist()
+def job_create_notification_log(doc, recipient, job_title):
+    subject = "New Job Interest for "+job_title
+    create_notification_log(doc, recipient, subject)
+    frappe.db.commit()
--- fosa_connect/fosa_connect/utils.py
+++ fosa_connect/fosa_connect/utils.py
@@ -1,29 +1,48 @@
 import frappe
 
+
 @frappe.whitelist()
 def create_job_interest_entry(job_id, student_id):
     try:
         member = frappe.get_doc("Member", {"email_id": student_id})
         # Check if a job interest entry already exists for the student and job
-        existing_entry = frappe.get_list(
-            "Job Interest",
-            filters={"job": job_id, "member": member.name},
-        )
-        if not existing_entry:
+        if not frappe.db.exists("Job Interest", {"job": job_id, "member": member.name}):
             # Create a new entry in the "Job Interest" DocType
             job_interest = frappe.new_doc("Job Interest")
             job_interest.job = job_id
-            job_interest.member = member
+            job_interest.member = member.name
             job_interest.posting_date = frappe.utils.nowdate()
             job_interest.save()
 
             # Return a success message
-            return "success"
+            frappe.msgprint("You have expressed interest in this job.")
         else:
             # A job interest entry already exists, so return an error message
-            return "Entry already exists"
+            frappe.throw("Job Interest was already created once")
 
     except Exception as e:
         # Handle any exceptions or errors that may occur during the process
         frappe.log_error(f"Error creating job interest entry: {str(e)}")
         return "Error"
+
+
+# Notification Function
+@frappe.whitelist()
+def create_notification_log(doc, recipient, subject, content=None, type=None):
+    """method is used to create notification log
+    args:
+        doc: document object
+        recipient: notification receiving user
+        subject: subject of notification log
+        type: type of the notification log"""
+    notification_log = frappe.new_doc("Notification Log")
+    notification_log.type = "Alert"
+    if type:
+        notification_log.type = type
+    notification_log.document_type = doc.doctype
+    notification_log.document_name = doc.name
+    notification_log.for_user = recipient
+    notification_log.subject = subject
+    if content:
+        notification_log.email_content = content
+    notification_log.save(ignore_permissions=True)
