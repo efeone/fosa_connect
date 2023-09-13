58094395bfb7d5fd1992cac916ad7f5722254f70
shamila <shamilabackerkp@gmail.com>
2023-09-13 15:39:04 +0530

    chore:Only published job can be viewed to students

--- fosa_connect/fosa_connect/doctype/job/job.py
+++ fosa_connect/fosa_connect/doctype/job/job.py
@@ -2,16 +2,19 @@ import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 class Job(WebsiteGenerator):
-	pass
+    pass
+
 @frappe.whitelist()
 def permission_query_conditions(user):
-	if not user:
-		user = frappe.session.user
-	user_roles = frappe.get_roles(user)
-	if "Administrator" in user_roles:
-		return None
-	if "Student" in user_roles:
-		conditions = """tabJob.published = 1"""
-		return conditions
+    if not user:
+        user = frappe.session.user
+    user_roles = frappe.get_roles(user)
 
-	return
+    if "Administrator" in user_roles:
+        return None
+
+    if "Student" in user_roles:
+        conditions = """tabJob.published = 1"""
+        return conditions
+
+    return None
