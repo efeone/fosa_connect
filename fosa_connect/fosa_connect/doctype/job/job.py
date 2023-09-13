f924c2463b8df1cc98e05554e69232bfd93e3e8e
shamila <shamilabackerkp@gmail.com>
2023-09-13 15:39:04 +0530

    feat:Only published job can be viewed by student.Not be viewed if it was disabled job

--- fosa_connect/fosa_connect/doctype/job/job.py
+++ fosa_connect/fosa_connect/doctype/job/job.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2023, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Job(Document):
 	pass
+
+
+
+@frappe.whitelist()
+def permission_query_conditions(user):
+	if not user:
+		user = frappe.session.user
+	user_roles = frappe.get_roles(user)
+	if user == "Administrator" in user_roles:
+		return None
+
+	if "Student" in user_roles:
+		conditions = """tabJob.published = 1"""
+		return conditions
+
+	return None  # Return None for other roles
