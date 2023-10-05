ad0e7b83626e4c57000cd0c0367547b2ef36b915
aswanikrishnap <applermon207@gmail.com>
2023-10-05 14:07:34 +0530

    fix:when_click_applynow_applied

--- fosa_connect/www/jobs/job.py
+++ fosa_connect/www/jobs/job.py
@@ -3,6 +3,7 @@ import frappe
 
 def get_context(context):
     context.roles = frappe.get_roles(frappe.session.user)
+    context.member = frappe.get_value("Member", {"email_id": frappe.session.user}, "name")
     context.jobs = frappe.get_doc('Job',frappe.form_dict.docname)
     return {
         "context": context
