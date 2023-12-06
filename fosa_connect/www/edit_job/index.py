8aebea81ac59cc030cc6d696c57ce666e904518d
aswanikrishnap <applermon207@gmail.com>
2023-12-06 12:02:36 +0530

    feat:added new fields in edit job

--- fosa_connect/www/edit_job/index.py
+++ fosa_connect/www/edit_job/index.py
@@ -6,11 +6,11 @@ def get_context(context):
     context.job_id = job_id
 
 @frappe.whitelist()
-def edit_job(job_id, job_title, qualification, responsibility, start_date, end_date, job_category, location, job_type, salary_info, job_description, organization, isPublished, disabled):
+def edit_job(job_id, job_title, qualification, organization_website, start_date, end_date, job_category, location, job_type, salary_info, job_description, organization, organization_type, isPublished,disabled):
     job=frappe.get_doc("Job",job_id)
     job.job_title = job_title
     job.qualification = qualification
-    job.responsibility = responsibility
+    job.organization_website = organization_website
     job.start_date = start_date
     job.last_date_to_apply = end_date
     job.job_category = job_category
@@ -19,6 +19,7 @@ def edit_job(job_id, job_title, qualification, responsibility, start_date, end_d
     job.salary_info = salary_info
     job.job_description = job_description
     job.organization_name = organization
+    job.organization_type = organization_type
     job.published = isPublished
     job.disabled = disabled
     job.save()
